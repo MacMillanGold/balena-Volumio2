c9492a35e5e8cf48f517e8be0d084ff8f6f4535c
Eugene Mirotin <emirotin@gmail.com>
2015-10-02 15:40:48 +0300

    resinify

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var expressInstance = require('./http/index.js');
 var expressApp = expressInstance.app;
 // Using port 3000 for the debug interface
-expressApp.set('port', 3000);
+expressApp.set('port', process.env.PORT || 80);
 
 var httpServer = expressApp.listen(expressApp.get('port'), function() {
 	console.log('Express server listening on port ' + httpServer.address().port);
