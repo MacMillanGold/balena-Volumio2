16e1d7014d89175624cb2ee2489df0d397d9260c
Eugene Mirotin <emirotin@gmail.com>
2015-10-05 00:40:49 +0300

    don't disable dbus

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Set host-name and enable-dbus
-sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
+sed -i "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
 
 # Restart service
 /etc/init.d/avahi-daemon restart
